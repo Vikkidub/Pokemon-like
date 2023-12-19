0a426750822ff042a5f1d3220194593dd9b5e217
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-12-19 10:55:51 +0100

    Add project files.

---
+++ ConsoleApp21/Pokemon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public Pokemon(string name, int health, int damage) {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+       public void Scratch(Pokemon actor, Pokemon receiver)
+        {
+            receiver.Health -= actor.Damage;
+        }
+       public void Growl(Pokemon actor)
+        {
+            actor.Damage ++;
+        }
+       public void Snooze(Pokemon actor)
+        {
+            actor.Health += 5;
+        }
+        public void OpponentAction(Pokemon a, Pokemon b)
+        {
+            var rand = new System.Random();
+            var randomNumber = rand.Next(1, 4);
+            if (randomNumber == 1) {
+                Scratch(a,b);
+                Console.WriteLine($"{a.Name} scratched {b.Name} for {a.Damage} damage");
+                Console.WriteLine($"{b.Name} has {b.Health} health left");
+            }
+            else if (randomNumber == 2){
+                Growl(a);
+                Console.WriteLine($"{a.Name} used growl. Its power grows..");
+            }
+            else if (randomNumber == 3)
+            {
+                Snooze(a);
+                Console.WriteLine($"{a.Name} went to sleep. It regained health");
+            }
+            else { Console.WriteLine("OpponentAction 'if' statement issue"); }
+        }
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp21;
+
+Encounter();
+void Encounter()
+{
+    var charmander = new Pokemon("Charmander", 5, 2);
+    var squirtle = new Pokemon("Squirtle", 4, 1);
+
+    Console.WriteLine("Wild Squirtle appeared.");
+    Console.WriteLine("Go Charmander!");
+    while (charmander.Health > 0 && squirtle.Health > 0)
+    {
+        Console.WriteLine("Scratch(1) Growl(2) Snooze(3)");
+        var input = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                charmander.Scratch(charmander, squirtle);
+                Console.WriteLine($"Charmander scratched Squirtle for {charmander.Damage} damage");
+                Console.WriteLine($"Squirtle has {squirtle.Health} health remaining");
+                Console.ReadLine();
+                squirtle.OpponentAction(squirtle, charmander);
+                Console.ReadLine();
+                break;
+            case "2":
+                charmander.Growl(charmander);
+                Console.WriteLine($"Charmander used growl. Its power grows..");
+                Console.ReadLine();
+                squirtle.OpponentAction(squirtle, charmander);
+                Console.ReadLine();
+                break;
+            case "3":
+                charmander.Snooze(charmander);
+                Console.WriteLine($"Charmander went to sleep. It regained health");
+                Console.ReadLine();
+                squirtle.OpponentAction(squirtle, charmander);
+                Console.ReadLine();
+                break;
+            default:
+                Console.WriteLine($"{input} is not a valid input. Enter a number 1,2 or 3");
+                break;
+        }
+    }
+    Console.WriteLine("The battle has concluded!");
+    Console.WriteLine($"Charmander has {charmander.Health} health remaining, while Squirtle has {squirtle.Health}");
+}
+
+Console.ReadLine();
